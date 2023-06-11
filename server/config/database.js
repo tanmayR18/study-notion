46bfb171f027435bdb8d1430e3343bf1b819d98a
tanmayrane18 <tanmayrane51@gmail.com>
2023-06-11 16:11:10 +0530

    database config set up in database.js

---
+++ server/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then( () => console.log("DB connected successfully"))
+    .catch( (err) => {
+        console.log("DB Connection Failed")
+        console.error(err)
+        process.exit(1);
+    })
+}
