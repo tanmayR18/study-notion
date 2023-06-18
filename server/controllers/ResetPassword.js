6b1f84358fac036fe82a8a47284d4be8de75c6b1
tanmayrane18 <tanmayrane51@gmail.com>
2023-06-18 13:39:30 +0530

    Functionality to reset password added

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -49,4 +49,57 @@ exports.resetPasswordToken = async(req,res) => {
     }
 }
 
+//resetPassword
+exports.resetPassword = async(req,res) => {
+    try{
+        //data fetch
+        const {password, confirmPassword, token} = req.body
 
+        //validation
+        if(password !== confirmPassword){
+            return res.json({
+                success:false,
+                message:'Password not matching',
+            });
+        }
+
+        //get userdetails from db using token
+        const userdetails = await User.findOne({token:token})
+        //if no enty- invalid
+        if(!userdetails) {
+            return res.json({
+                success:false,
+                message:'Token is invalid',
+            });
+        }
+        //token time check
+        if(userdetails.resetPasswordExpires < Date.now()){
+            return res.json({
+                success:false,
+                message:'Token is expired, please regenerate your token',
+            });
+        }
+        //hase password
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        //password update
+        await User.findOneAndUpdate(
+            {token:token},
+            {password:hashedPassword},
+            {new:true}
+        )
+
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:'password rest successfully',
+        })
+
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Something went wrong while sending reset pwd mail'
+        })
+    }
+}
