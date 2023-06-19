77e64eb3621f349bcd34e0ff9ecf1ee6a59e8105
tanmayrane18 <tanmayrane51@gmail.com>
2023-06-19 23:03:18 +0530

    Authentication added

---
+++ server/middlewares/auth.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken')
+require('dotenv')
+const User = require('../model/User')
+
+//auth 
+exports.auth = async(req, res, next) => {
+    try{
+        //extract token
+        const token = req.cookies.token 
+                        || req.body.token
+                        || req.header("Authorisation").replace("Brearer ","")
+        //if token missing, then return response
+        if(token){
+            return res.status(401).json({
+                sucess:false,
+                message:"Token is missing"
+            })
+        }
+
+        //verify the token
+        try {
+            const decode = jwt.verify(token,process.env.JWT_SECRET)
+            console.log(decode)
+            req.user = decode
+        } catch(error){
+            //verification - issue
+            return res.status(401).json({
+                success:false,
+                message:'token is invalid',
+            });
+
+        next()
+        }
+    }
+    catch(error){
+        return res.status(401).json({
+            success:false,
+            message:'Something went wrong while validating the token',
+        });
+    }
+}
+
+
