9422e18b89124bd3bdc5fce04e303c23a3037519
tanmayrane18 <tanmayrane51@gmail.com>
2023-06-19 23:09:21 +0530

    Authorization for student added

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -41,3 +41,22 @@ exports.auth = async(req, res, next) => {
 }
 
 
+//isStudent
+exports.isStudent = async(req,res) => {
+    try{
+        if(req.user.accountType !== "Student") {
+            return res.status(401).json({
+                sucess:false,
+                message:"This is a protected route for Student only"
+            })
+        }
+        next()
+    } catch(error){
+        return res.status(500).json({
+            success:false,
+            message:'User role cannot be verified, please try again'
+        })
+    }
+}
+
+
