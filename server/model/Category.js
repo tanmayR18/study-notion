d27cbc0b5c47d4d157a7f0cface6c5a9d6883670
tanmayrane18 <tanmayrane51@gmail.com>
2023-06-20 17:11:33 +0530

    Issue of tag-categoty fixed

--- server/model/Category.js
+++ server/model/Category.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const tagsSchema = new mongoose.Schema({
+const categorySchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -15,4 +15,4 @@ const tagsSchema = new mongoose.Schema({
     }]
 })
 
-module.exports = mongoose.model("Tag",tagsSchema)
+module.exports = mongoose.model("Category",categorySchema)
--- server/model/Course.js
+++ server/model/Course.js
@@ -33,9 +33,9 @@ const courseSchema = new mongoose.Schema({
     thumbnail: {
         type: String,
     },
-    tag: {
+    category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Tag",
+        ref: "Category",
     },
     studentEnrolled: [
         {
