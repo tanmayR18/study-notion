30f38e5ae3e06035d73525a3f052679cb02ca0e4
tanmayrane18 <tanmayrane51@gmail.com>
2023-06-20 17:29:22 +0530

    Functionality for updating sections added

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -40,3 +40,33 @@ exports.createSection = async(req, res) => {
         });
     }
 }
+
+
+exports.updateSection = async(req, res) => {
+    try{
+        //data input
+        const {sectionName, sectionId} = req.body
+        //data validation
+        if(!sectionName || !sectionId) {
+            return res.status(400).json({
+                success:false,
+                message:'Missing Properties',
+            });
+        }
+        //update data
+        const section = await Section.findByIdAndUpdate(sectionId,{sectionName},{new:true})
+        //return res
+        return res.status(200).json({
+            success:true,
+            message:'Section Updated Successfully',
+        });
+    } catch(error) {
+        return res.status(500).json({
+            success:false,
+            message:"Unable to update Section, please try again",
+            error:error.message,
+        });
+    }
+}
+
+
