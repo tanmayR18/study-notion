ce49650a721eb7ddcc55c2aca0eb3d7b6c3d2705
tanmayrane18 <tanmayrane51@gmail.com>
2023-06-20 12:04:01 +0530

    funcitonality to add tags added

---
+++ server/controllers/Tag.js
@@ -0,0 +1,36 @@
+const Tag = require('../model/Tags')
+
+//create Tags
+
+exports.createTag = async (req,res) => {
+    try{
+        //fetch data 
+        const {name, description} = req.body
+        //validation
+        if(!name || !description){
+            return res.json(400).json({
+                success:true,
+                message:"All fields are required"
+            })
+        }
+
+        //create entry in DB
+        const tagDetails = await Tag.create({
+            name:name,
+            description:description
+        })
+        console.log(tagDetails)
+
+        return res.status(200).json({
+            success:true,
+            message:"Tag created Successdfully"
+        })
+    } catch(error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+
