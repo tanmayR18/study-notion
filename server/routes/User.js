5918b5d4370bef1772f2ffc540cdb304ffb79b0e
tanmayrane18 <tanmayrane51@gmail.com>
2023-06-22 17:08:44 +0530

    routes for user added

---
+++ server/routes/User.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const router = express.Router()
+
+const {
+  login,
+  signup,
+  sendotp,
+  changePassword,
+} = require("../controllers/Auth")
+
+const {
+    resetPasswordToken,
+    resetPassword,
+  } = require("../controllers/ResetPassword")
+
+  const { auth } = require("../middleware/auth")
+
+
+// Route for user login
+router.post("/login", login)
+
+// Route for user signup
+router.post("/signup", signup)
+
+// Route for sending OTP to the user's email
+router.post("/sendotp", sendotp)
+
+// Route for Changing the password
+router.post("/changepassword", auth, changePassword)
+
+
+// Route for generating a reset password token
+router.post("/reset-password-token", resetPasswordToken)
+
+// Route for resetting user's password after verification
+router.post("/reset-password", resetPassword)
+
+// Export the router for use in the main application
+module.exports = router
