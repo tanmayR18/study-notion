fbdd33d26a6adf1e2707a23a0a94613815d29744
tanmayrane18 <tanmayrane51@gmail.com>
2023-06-28 21:23:13 +0530

    centralized store for data created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from '../src/pages/Home'
 import Navbar from "./components/common/Navbar";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
