5c89c5b7b5959975597b7dabb3755d7000b6a6fe
tanmayrane18 <tanmayrane51@gmail.com>
2023-09-20 22:45:42 +0530

    About section added only the responsiveness is to be added

--- src/components/core/AboutPage/Quote.jsx
+++ src/components/core/AboutPage/Quote.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import HighlightText from '../HomePage/HighlightText'
+import {BiSolidQuoteLeft, BiSolidQuoteRight} from "react-icons/bi"
 
 const Quote = () => {
   return (
-    <div>
-        We are passionate about revolutionizing the way we learn. Our innovative platform
-        <HighlightText text={"combines technology"} />
-        <span className='text-brown-500'>
-            expertise
-        </span>
-        , and community to create and
-        <span className=' text-caribbeangreen-100'>
-            {" "}
-            unparalleled educational experience.
-        </span>
+    <div className=' py-20 mt-20 text-4xl w-10/12 mx-auto font-bold'>
+        <center className=''>
+            <BiSolidQuoteLeft className=' text-richblack-600'/>
+            We are passionate about revolutionizing the way we learn. Our innovative platform
+            <HighlightText text={"Combine Technology"} direction={"bg-gradient-to-b"} gradient={" from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB]"} />
+            <HighlightText text={"expertise"} direction={"bg-gradient-to-b"} gradient={" from-[#FF512F] to-[#F09819]"} />
+            , and community to create and
+            <HighlightText text={"unparalleled educational experience."} direction={"bg-gradient-to-b"} gradient={" from-[#E65C00] to-[#F9D423]"} />
+            <BiSolidQuoteRight className=' text-richblack-600'/>
+        </center>
     </div>
   )
 }
--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -8,21 +8,24 @@ import Stats from '../components/core/AboutPage/Stats'
 import LearningGrid from '../components/core/AboutPage/LearningGrid'
 import ContactFormSection from '../components/core/AboutPage/ContactFormSection'
 import Footer from '../components/common/Footer'
+import FoundingStory from "../assets/Images/FoundingStory.png"
 
 const Aboutus = () => {
   return (
-    <div className=' mx-auto mt-[100px] text-white'>
+    <div className=' text-white'>
         {/* Section 1 */}
-        <section>
-            <div>
-                <header>
-                Driving Innovation in Online Education for a 
-                <HighlightText text={"Brighter Future"} />
-                <p>Studynotion is at the forefront of driving innovation in online education. We're passionate about creating a brighter future by offering cutting-edge courses, leveraging emerging technologies, and nurturing a vibrant learning community.</p>
+        <section className=' bg-richblack-700 '>
+            <div className='w-11/12 mx-auto pt-[80px] flex flex-col items-center'>
+                <p className=' text-richblack-200 text-base'>About Us</p>
+                <header className=' mt-[50px] mb-[40px] flex flex-col text-[36px] leading-10 font-semibold items-center'> 
+                    <span>Driving Innovation in Online Education for a</span>
+                    <HighlightText text={"Brighter Future"} direction={"bg-gradient-to-b"} gradient={" from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB]"} />
                 </header>
+                <p className=' w-2/3 text-richblack-300'><center>Studynotion is at the forefront of driving innovation in online education. We're passionate about creating a brighter future by offering cutting-edge courses, leveraging emerging technologies, and nurturing a vibrant learning community.</center></p>
+                
             
-                <div className='flex gap-x-3 mx-auto'>
-                    <img alt='' src={BannerImage1} />
+                <div className='flex flex-col lg:flex-row flex-wrap gap-6 mx-auto lg:translate-y-14'>
+                    <img alt=''  src={BannerImage1} />
                     <img alt='' src={BannerImage2} />
                     <img alt='' src={BannerImage3} />
                 </div>
@@ -36,33 +39,34 @@ const Aboutus = () => {
 
         {/* Section 3 */}
         <section>
-            <div className='flex flex-col'>
+            <div className='flex flex-col border-t border-richblack-700'>
             {/* founding story wala div */}
                 <div className='flex'>
                 {/* Founding story left box */}
-                <div>
-                    <h1>
-                        Our Founding Story
-                    </h1>
-                    <p>Our e-learning platform was born out of a shared vision and passion for transforming education. It all began with a group of educators, technologists, and lifelong learners who recognized the need for accessible, flexible, and high-quality learning opportunities in a rapidly evolving digital world.</p>
+                <div className=' flex flex-col lg:flex-row py-24 px-32 gap-24'>
+                    <div>
+                        <h1 className=' text-[36px]'>
+                        <HighlightText text={"Brighter Future"} direction={"bg-gradient-to-br"} gradient={" from-[#833AB4] via-[#FD1D1D] to-[#FCB045]"} />
+                        </h1>
+                        <p className=' text-richblack-300 mt-6 mb-4'>Our e-learning platform was born out of a shared vision and passion for transforming education. It all began with a group of educators, technologists, and lifelong learners who recognized the need for accessible, flexible, and high-quality learning opportunities in a rapidly evolving digital world.</p>
 
-                    <p>As experienced educators ourselves, we witnessed firsthand the limitations and challenges of traditional education systems. We believed that education should not be confined to the walls of a classroom or restricted by geographical boundaries. We envisioned a platform that could bridge these gaps and empower individuals from all walks of life to unlock their full potential.</p>
+                        <p className=' text-richblack-300'>As experienced educators ourselves, we witnessed firsthand the limitations and challenges of traditional education systems. We believed that education should not be confined to the walls of a classroom or restricted by geographical boundaries. We envisioned a platform that could bridge these gaps and empower individuals from all walks of life to unlock their full potential.</p>
+                    </div>
+                    <img className='w-1/2 ' src={FoundingStory} alt='' />
                 </div>
                 </div>
                 {/* vision and mission wala parent div */}
                 {/* Left box */}
-                <div>
-                    <h1>
-                        Our Vision
-                    </h1>
-                    <p>With this vision in mind, we set out on a journey to create an e-learning platform that would revolutionize the way people learn. Our team of dedicated experts worked tirelessly to develop a robust and intuitive platform that combines cutting-edge technology with engaging content, fostering a dynamic and interactive learning experience.</p>
-                </div>
-                {/* Right box */}
-                <div>
-                    <h1>
-                        Our Mission
-                    </h1>
-                    <p>Our mission goes beyond just delivering courses online. We wanted to create a vibrant community of learners, where individuals can connect, collaborate, and learn from one another. We believe that knowledge thrives in an environment of sharing and dialogue, and we foster this spirit of collaboration through forums, live sessions, and networking opportunities.</p>
+                <div className='flex flex-col lg:flex-row py-24 px-32 gap-24'>
+                    <div>
+                        <HighlightText  text={"Our Vision"} direction={"bg-gradient-to-b text-[36px]"} gradient={" from-[#E65C00] to-[#F9D423]"} />
+                        <p className=' text-richblack-300 mt-6'>With this vision in mind, we set out on a journey to create an e-learning platform that would revolutionize the way people learn. Our team of dedicated experts worked tirelessly to develop a robust and intuitive platform that combines cutting-edge technology with engaging content, fostering a dynamic and interactive learning experience.</p>
+                    </div>
+                    {/* Right box */}
+                    <div>
+                        <HighlightText text={"Our Mission"} direction={"bg-gradient-to-br text-[36px]"} gradient={" from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB]"} />
+                        <p className=' text-richblack-300 mt-6'>Our mission goes beyond just delivering courses online. We wanted to create a vibrant community of learners, where individuals can connect, collaborate, and learn from one another. We believe that knowledge thrives in an environment of sharing and dialogue, and we foster this spirit of collaboration through forums, live sessions, and networking opportunities.</p>
+                    </div>
                 </div>
             </div>
         </section>
@@ -71,7 +75,7 @@ const Aboutus = () => {
         <Stats/>
 
         {/* Section 5 */}
-        <section className=' mx-auto flex flex-col items-center justify-between gap-5 mb-[140px]'>
+        <section className=' mx-auto flex flex-col items-center justify-between gap-5 py-[90px]'>
         <LearningGrid/>
         <ContactFormSection/>
         </section>
