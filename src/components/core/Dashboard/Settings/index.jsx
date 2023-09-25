f34e5e88ae2186fd9c5f68376860ec0241d22627
tanmayrane18 <tanmayrane51@gmail.com>
2023-09-25 16:10:34 +0530

    Profile update section added

--- src/components/core/Dashboard/Settings/index.jsx
+++ src/components/core/Dashboard/Settings/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import ChangeProfilePicture from './ChangeProfilePicture'
+import EditProfile from './EditProfile'
+import UpdatePassword from './UpdatePassword'
+import DeleteAccount from './DeleteAccount'
 
 const Settings = () => {
   return (
     <div>
-        Settings
+        <h1 className=' mb-14 text-3xl font-semibold text-richblack-5'>
+            Edit Profile
+        </h1>
+        
+        {/* Change Profile Picture */}
+        <ChangeProfilePicture/>
+        {/* Profile */}
+        <EditProfile/>
+        {/* Password */}
+        <UpdatePassword/>
+        {/* Delete Account */}
+        <DeleteAccount/>
     </div>
   )
 }
