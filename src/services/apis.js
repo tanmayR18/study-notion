610f4f8ee58687c1ab07439476e4ab74016dcd2f
tanmayrane18 <tanmayrane51@gmail.com>
2023-10-05 09:57:20 +0530

    Frontend api for payment gateway added]

--- src/services/apis.js
+++ src/services/apis.js
@@ -27,7 +27,7 @@ export const studentEndpoints = {
     COURSE_PAYMENT_API: BASE_URL + "/payment/capturePayment",
     COURSE_VERIFY_API: BASE_URL + "/payment/verifyPayment",
     SEND_PAYMENT_SUCCESS_EMAIL_API: BASE_URL + "/payment/sendPaymentSuccessEmail",
-  }
+}
 
 // COURSE ENDPOINTS
 export const courseEndpoints = {
