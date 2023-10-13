f25ab32c7615c8608b7acdd161b6c14b2d6f4da5
tanmayrane18 <tanmayrane51@gmail.com>
2023-10-13 12:08:50 +0530

    Navbar modified

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -10,6 +10,7 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import { apiConnector } from '../../services/apiconnector'
 import { categories } from '../../services/apis'
+import { AiOutlineMenu } from "react-icons/ai"
 
 
 // const subLinks = [
@@ -61,7 +62,7 @@ const Navbar = () => {
     // }
 
   return (
-    <div className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${location.pathname === "/" ? "" : "bg-richblack-800"}`}>
+    <div className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${location.pathname === "/" ? "" : "bg-richblack-800"} transition-all duration-200`}>
         <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
             {/* Image */}
             <Link to={"/"}>
@@ -71,7 +72,7 @@ const Navbar = () => {
             </Link>
 
             {/* Nav Links */}
-            <nav>
+            <nav className=' hidden md:block'>
                 <ul className='flex gap-x-6 text-richblack-25'>
                     {
                         NavbarLinks.map( (link, index) => (
@@ -79,18 +80,18 @@ const Navbar = () => {
                                 {
                                     link.title === "Catalog" ? 
                                     (
-                                        <div className=' relative flex items-center gap-2 group'>
+                                        <div className='relative flex cursor-pointer items-center gap-2 group'>
                                             <p>{link.title}</p>
                                             <RiArrowDropDownLine/>
-
-                                            <div className=' invisible absolute left-[50%] translate-x-[-50%]
-                                             translate-y-[30%] top-[-30%] group-hover:translate-y-[20%] group-hover:top-[-20%] flex flex-col rounded-md bg-richblack-5
-                                             p-4 text-richblack-900 opacity-0 transition-all duration-200
-                                              group-hover:opacity-100 group-hover:visible lg:w-[300px] z-10'>
+                                            {/* left-[50%] translate-x-[-50%] translate-y-[30%] top-[-30%] */}
+                                            <div className=' invisible absolute left-[50%] top-[50%] z-[1000] w-[200px] translate-x-[-50%] translate-y-[3rem]
+                                            group-hover:translate-y-[1.65em] flex flex-col rounded-lg bg-richblack-5
+                                             p-4 text-richblack-900 opacity-0 transition-all duration-300
+                                              group-hover:opacity-100 group-hover:visible lg:w-[300px]'>
 
                                             <div className='absolute left-[50%] top-0 translate-x-[80%]
-                                            translate-y-[-45%] h-6 w-6 rotate-45 rounded bg-richblack-5'>
-
+                                            translate-y-[-40%] h-6 w-6 rotate-45 select-none rounded bg-richblack-5'>
+                                                
                                             </div>
 
                                             <div className='flex flex-col gap-2'>
@@ -98,10 +99,10 @@ const Navbar = () => {
                                                 subLinks.length ? (
                                                     subLinks.map( (subLink, index) => (
                                                         <Link to={"/catalog/" + subLink.name.split(" ").join("-").toLowerCase()} key={index}>
-                                                            <p className=' hover:bg-richblack-25 rounded-md py-4 px-4 font-semibold'>{subLink.name}</p>
+                                                            <p className=' hover:bg-richblack-50 bg-transparent rounded-lg py-4 px-4 font-semibold'>{subLink.name}</p>
                                                         </Link>
                                                     ))
-                                                ) : (<div className='text-4xl text-richblack-900'>Not data found</div>)
+                                                ) : (<div className='text-richblack-900 text-center'>No data found</div>)
                                             }
                                             </div>
                                             
@@ -128,14 +129,14 @@ const Navbar = () => {
             </nav>
 
             {/* Login/Signup/Dashboard */}
-            <div className='flex gap-x-4 items-center text-white'>
+            <div className=' hidden md:flex gap-x-4 items-center text-white'>
                 {
                     user && user?.accountType !== "Instructor" && (
                         <Link to={"/dashboard/cart"} className='relative'>
-                            <AiOutlineShoppingCart size={20}/>
+                            <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
                             {
                                 totalItems > 0 && (
-                                    <span className=' grid place-items-center animate-bounce absolute -top-1 -right-1 bg-yellow-100 text-richblack-900 w-4 h-4 rounded-full'>
+                                    <span className=' grid place-items-center animate-bounce absolute -top-1 -right-1 bg-yellow-100 text-richblack-900 w-5 h-5 rounded-full'>
                                         <p className=' text-xs'>{totalItems}</p>
                                     </span>
                                 )
@@ -146,7 +147,7 @@ const Navbar = () => {
                 {
                     token === null && (
                         <Link to ="/login">
-                            <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md'>
+                            <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-[8px]'>
                                 Log In
                             </button>
                         </Link>
@@ -155,7 +156,7 @@ const Navbar = () => {
                 {
                     token === null && (
                         <Link to ="/signup">
-                            <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md'>
+                            <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-[8px]'>
                                 Sign Up
                             </button>
                         </Link>
@@ -165,6 +166,9 @@ const Navbar = () => {
                     token !== null && <ProfileDropDown/>
                 }
             </div>
+            <button className=' mr-4 md:hidden'>    
+                <AiOutlineMenu fontSize={24} fill="#AFB2BF" />
+            </button>
         </div>
     </div>
   )
